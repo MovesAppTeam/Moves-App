bcbe7b10ac9341303b1b7f27bdba2b73ce4edf1a
Ahmir Postell <postell.ahmir@gmail.com>
2022-12-15 21:29:26 -0500

    Implemented Signin and Signup w/ Firebase

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,24 +1,25 @@
+import 'package:application/screens/Signin_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
-
-  print("hello world");
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello World")
-          )
-        )
-      )
-    );
-
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
-
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext) {
+    return MaterialApp(
+      title: 'flutter demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SignInScreen(),
+    );
+  }
+}
---
+++ application/lib/screens/signin_screen.dart
@@ -0,0 +1,108 @@
+import 'package:application/screens/home_screen.dart';
+import 'package:application/utils/color_utils.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+
+import 'package:flutter/material.dart';
+
+import '../reusable_widgets/reusable_widget.dart';
+import 'signup_screen.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+              hexStringToColor("57E2E5"),
+              hexStringToColor("6A7FDB"),
+              hexStringToColor("45CB85")
+            ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+            child: SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(
+                        20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+                    child: Column(
+                      children: <Widget>[
+                        logoWidget("assets/juncture-logo.png"),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        reusableTextField("Enter Email", Icons.person_outline,
+                            false, _emailTextController),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        reusableTextField("Enter Password", Icons.lock_outline,
+                            true, _passwordTextController),
+                        const SizedBox(height: 20),
+                        signInSignUpButton(context, true, () {
+                          FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: _emailTextController.text,
+                                  password: _passwordTextController.text)
+                              .then((value) {
+                            print("Logged in successfully");
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeScreen()));
+                          }).onError((error, stackTrace) {
+                            print("Error ${error.toString()}");
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                              /// need to set following properties for best effect of awesome_snackbar_content
+                              elevation: 0,
+                              behavior: SnackBarBehavior.floating,
+                              backgroundColor: Colors.transparent,
+                              content: AwesomeSnackbarContent(
+                                title: 'On Snap!',
+                                message:
+                                    "The credentials entered must be incorrect!",
+
+                                /// change contentType to ContentType.success, ContentType.warning or ContentType.help for variants
+                                contentType: ContentType.failure,
+                  ),
+                )
+                            );
+
+
+                          });
+                        }),
+                        signUpOption()
+                      ],
+                    )))));
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account? ",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignUpScreen()));
+          },
+          child: const Text(
+            "Sign Up",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
